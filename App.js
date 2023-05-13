f06bc710cac1dedd670706b9ff7eac0f52b9667c
tania.alonso <tania.alonso@estudiantat.upc.edu>
2023-05-13 14:49:53 +0200

    hh

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
-import Settings from './screens/inici';
+import Settings from './screens/Inici';
+import Inici from './screens/Inici';
 
 export default function App() {
-    <View><Text>Holii bb</Text></View>
+    return (
+    
+    <View>
+        <Text>Holii bb</Text>
+        <Inici></Inici>
+    
+    
+    </View>)
 }
---
+++ screens/Inici.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+
+export default function Inici(props) {
+  const [username, setUsername] = useState('');
+
+  const handleTextChangeUsername = (text) => {
+    setUsername(text);
+  };
+
+  const handleJocPress = () => {
+    navigation.navigate('joc');
+  };
+
+  return (
+    <View>
+      <Text> holaaa </Text>
+        <TextInput
+        style={styles.inputUser}
+        placeholder="Username"
+        placeholderTextColor="#FFFFFF"
+        value={username}
+        onChangeText={handleTextChangeUsername}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleJocPress}>
+            <Text> play </Text>
+        </TouchableOpacity>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  inputUser: {
+    height: 40,
+    marginTop: '15%',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    color: 'white',
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+});
